4267a96132acd91cb6030aadbf12ba0e2d2c5c0c
JustEvil <71156616+EvilG-MC@users.noreply.github.com>
2025-12-27 16:14:55 -0600

    use keys instead of list

--- src/middlewares/staff.middleware.ts
+++ src/middlewares/staff.middleware.ts
@@ -2,7 +2,7 @@ import { createMiddleware } from "seyfert";
  
 export const staffMiddleware = createMiddleware<void>(
     async (middle) => {
-        if (!(await middle.context.member?.roles.list())?.map(r => r.id).includes('1453058023625855132')) return middle.context.write({ content: 'Solo **los creadores del bot** pueden ejecutar este comando.' });
+        if (!middle.context.member?.roles.keys.includes('1453058023625855132')) return middle.context.write({ content: 'Solo **los creadores del bot** pueden ejecutar este comando.' });
 
         return middle.next();
     }
